20680369c808209660bd6d77c531f66566e22e0f
Siva Prasad Bhushetty <sivaprasad1110@gmail.com>
2016-07-17 21:26:19 -0400

    commit on 07/17/2016 9.26 PM

--- skeleton-web-services/src/main/java/com/example/SkeletonWebServicesApplication.java
+++ skeleton-web-services/src/main/java/com/example/SkeletonWebServicesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SkeletonWebServicesApplication {
 
 	//my first commit
+	//my second commit
 	public static void main(String[] args) {
 		SpringApplication.run(SkeletonWebServicesApplication.class, args);
 	}
